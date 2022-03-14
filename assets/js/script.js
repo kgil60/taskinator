a0bff5afb817f69f956bc4b8513aeda93fb185b7
Kenny <gilvaryke@gmail.com>
2022-03-14 00:35:53 -0400

    added ability to add tasks with name and type

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,18 @@ let formEl = document.getElementById("task-form");
 
 function addTaskHandler(e) {
     e.preventDefault();
-    
+
+    let taskNameInput = document.querySelector("input[name='task-name']").value;
+    let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
     let taskItem = document.createElement("li");
-    taskItem.textContent = "A new task has been added";
     taskItem.className = 'task-item';
+
+    let taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = `<h3 class='task-name'>${taskNameInput}</h3><span class='task-type'>${taskTypeInput}</span>`;
+    taskItem.appendChild(taskInfoEl);
+
     taskList.appendChild(taskItem);
 }
 
